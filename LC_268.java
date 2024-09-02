b48dc556515b7fc218a899d598890205f3934102
Saurabh Patel <saurabhpatel4589@gmail.com>
2024-09-02 22:10:12 +0530

    LEETCODE QUESION

--- LC_268.java
+++ LC_268.java
@@ -1,17 +1,17 @@
 class LC_268 {
     public int brute_missingNumber(int[] nums) {
-        int sum_1=0;
-        int sum_2=0;
-        for(int i=0;i<nums.length+1;i++){
-            sum_1+=i;
+        int sum_1 = 0;
+        int sum_2 = 0;
+        for (int i = 0; i < nums.length + 1; i++) {
+            sum_1 += i;
         }
-        for(int i=0;i<nums.length;i++){
-            sum_2+=nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            sum_2 += nums[i];
         }
-        int difference = sum_1-sum_2;
-        return  difference;
+        int difference = sum_1 - sum_2;
+        return difference;
     }
-    
+
     public int optimal_missingNumber(int[] nums) {
         int n = nums.length;
         int sum_1 = n * (n + 1) / 2;
@@ -23,4 +23,17 @@ class LC_268 {
         int difference = sum_1 - sum_2;
         return difference;
     }
+
+    public int most_optimal_missingNumber(int[] nums) {
+
+        int N = nums.length;
+        int xor_1 = 0, xor_2 = 0;
+        for (int i = 0; i < N; i++) {
+            xor_2 = xor_2 ^ nums[i];
+            xor_1 = xor_1 ^ (i);
+        }
+        xor_1 = xor_1 ^ N;
+        return xor_1 ^ xor_2;
+    }
+
 }
