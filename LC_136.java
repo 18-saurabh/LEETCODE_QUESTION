a76860ee74aa81200b46210c5662182564d8c753
Saurabh Patel <saurabhpatel4589@gmail.com>
2024-09-03 22:11:09 +0530

    LEETCODE QUESTION

---
+++ LC_136.java
@@ -0,0 +1,30 @@
+public class LC_136 {
+    public static int brute_AppearingOnes(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            int cnt = 0; // Reset 'cnt' for each new element
+            int num = a[i]; // Current element to check
+            for (int j = 0; j < a.length; j++) {
+                if (a[j] == num) {
+                    cnt++; // Increment count if match is found
+                }
+            }
+            if (cnt == 1) {
+                return num; // Return the element if it appears exactly once
+            }
+        }
+        return 0; // Return 0 if no unique element is found
+    }
+   
+    public static int optimal_AppearingOnes(int nums[]) {
+        int xor = 0; // Initialize a variable 'xor' to 0. This will hold the result of XOR
+                     // operations.
+
+        // Loop through each number in the array
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i]; // Apply XOR between 'xor' and the current element 'nums[i]'
+        }
+
+        return xor; // Return the result after all XOR operations are done
+    }
+
+}
