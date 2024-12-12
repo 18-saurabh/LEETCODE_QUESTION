f84c2f6e2b975376fcd435127fc0323e58ba21dd
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2024-12-12 19:20:47 +0530

    LEETCODE QUESTIONS

--- LC_704.java
+++ LC_704.java
@@ -1,9 +1,26 @@
 public class LC_704 {
-    public int search(int[] nums, int target) {
+    public int search(int[] nums, int target) { // Linear search
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
                 return i;
             }
+        }
+        return -1;
+    }
+  
+    public int bs_search(int[] nums, int target) {        //Binary search
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;// oveflow condition where the low and high
+            // could stand on int max so to stop it used this.
+            if (nums[mid] == target)
+                return mid;
+            else if (target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
         return -1;
     }
