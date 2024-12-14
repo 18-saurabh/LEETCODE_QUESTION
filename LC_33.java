987af81b85d9824f917b477494f027b9b470d001
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2024-12-14 12:11:02 +0530

    LEETCODE QUESTION

---
+++ LC_33.java
@@ -0,0 +1,29 @@
+public class LC_33 {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            // IF left is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // IF right is sorted
+            else {
+                if (target <= nums[high] && target >= nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
