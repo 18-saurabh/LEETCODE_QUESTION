d33ce4db0103e91bdc84cc7fa3d1f513b4429b43
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2024-12-30 20:19:29 +0530

    LEETCODE QUESTIONS

---
+++ LC_19.java
@@ -0,0 +1,57 @@
+public class LC_19 {
+    static class ListNode{
+        int data;
+        ListNode next;
+        ListNode(int data,ListNode next){
+            this.data=data;
+            this.next=next;
+        }
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static int lengthOfLL(ListNode head) {
+        ListNode temp = head;
+        int length = 0;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || (head.next == null && n == 1)) {
+            return null;
+        }
+        int length = lengthOfLL(head);
+        int result = length - n;
+        ListNode temp = head;
+        if (length == n)
+            return head.next;
+        while (temp != null) {
+            result--;
+            if (result == 0) {
+                break;
+            }
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+    public ListNode optimal_removeNthFromEnd(ListNode head, int n) {
+        ListNode slow=head;
+        ListNode fast=head;
+        for(int i=1;i<=n;i++){
+            fast=fast.next;
+        }
+        if(fast==null)return head.next;
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
