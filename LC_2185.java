a3dbae51220c97a8c0d25fe3411a2ed707853522
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-09 19:47:54 +0530

    LEETCODE QUESTIONS

---
+++ LC_2185.java
@@ -0,0 +1,17 @@
+public class LC_2185 {
+     public int prefixCount(String[] words, String pref) {
+        int n = words.length;
+        int cnt = 0;
+        int len1 = pref.length();// prefix length
+        for (int i = 0; i < n; i++) {
+            int len2 = words[i].length();
+            if (len2 < len1) {
+                continue; // if String at words[i] length is smaler then the prefix then skip that part;
+            }
+            if (words[i].startsWith(pref)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
