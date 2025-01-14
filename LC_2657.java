dcf3a0b24036efc7977f8265d2efb1642636f1c4
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-14 19:46:24 +0530

    LEETCODE QUESTION

---
+++ LC_2657.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+public class LC_2657 {
+     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n=A.length;
+        int[]c=new int[n];
+        HashSet<Integer>seen=new HashSet<>();
+        int counter=0;
+        for(int i=0;i<n;i++){
+            if(seen.contains(A[i])){
+                counter++;
+            }
+            else{
+                seen.add(A[i]);
+            }
+            if(seen.contains(B[i])){
+                counter++;
+            }
+            else{
+                seen.add(B[i]);
+            }
+            c[i]=counter;
+        }
+        return c;
+    }
+}
