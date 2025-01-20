a1e4938d7a395609d97b319632aa5c18e37b3b37
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-20 19:03:18 +0530

    LEETCODE QUESTIONS

---
+++ LC_50.java
@@ -0,0 +1,19 @@
+public class LC_50 {
+    public double powX(double x,int n){
+        long nn=n;
+        double ans=1.0;
+        if(nn<0)nn=-1*nn;
+        while(nn>0){
+            if (nn%2==1) {
+                ans*=x;
+                nn=nn-1;
+            }
+            else{
+                x*=x;
+                nn=nn/2;
+            }
+        }
+        if(n<0)ans=(double)(1.0)/(double)(ans);
+        return ans;
+    }
+}
