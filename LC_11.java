161d5ef97cf9db02c22922181a1b7f8296a9c416
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-21 20:28:45 +0530

    LEETCODE QUESTIONS

---
+++ LC_11.java
@@ -0,0 +1,19 @@
+public class LC_11{
+public int maxArea(int[] height) {
+    int n=height.length;
+       int left=0;
+       int right=n-1;
+       int maxArea=0;
+       while(left<right){
+        int currentArea=Math.min(height[left],height[right])*(right-left);
+        maxArea=Math.max(maxArea,currentArea);
+        if(height[left]<height[right]){
+            left++;
+        }
+        else{
+            right--;
+        }
+       }
+       return maxArea;
+    }
+}
