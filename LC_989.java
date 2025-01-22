a4430fff84f37aad405344f0f9c08113547982df
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-22 19:01:17 +0530

    LEETCODE QUESTONS

---
+++ LC_989.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LC_989 {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+        int carry = k;
+        int n = num.length;
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = num[i] + carry;
+            result.add(sum % 10);
+            carry = sum / 10;  
+        }
+        while (carry > 0) {
+            result.add(carry % 10);
+            carry /= 10;
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
