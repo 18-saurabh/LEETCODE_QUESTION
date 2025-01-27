1d0121f51b696adcba072066828c5797f221e947
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-27 21:27:22 +0530

    LEETCODE QUESTIONS

---
+++ LC_231.java
@@ -0,0 +1,11 @@
+class LC_231{
+       public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        if((n & (n-1))==0){
+            return true;
+        }
+        return false;
+    }
+}
