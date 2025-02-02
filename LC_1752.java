582b6d629db306078659be7880944ae3c5025dc8
18-saurabh <saurabhpatel4589@gmail.com>
2025-02-02 15:46:41 +0530

    LEETCODE QUESTION

---
+++ LC_1752.java
@@ -0,0 +1,19 @@
+class LC_1752{
+    public boolean check(int[] nums) {
+        int countBreaks = 0;
+        int n = nums.length;
+        
+        // Check for breaks in the array
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                countBreaks++;
+            }
+            // If there is more than one break, return false
+            if (countBreaks > 1) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
