a5faffd9e50a8f78f3904dfe6a2ce2a66beb65ca
18-saurabh <saurabhpatel4589@gmail.com>
2025-02-03 21:45:54 +0530

    LEETCODE QUESTION

---
+++ LC_3105.java
@@ -0,0 +1,25 @@
+public class LC_3105 {
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 1;
+
+        int maxLen = 1;
+        int incLen = 1, decLen = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                incLen++;
+                decLen = 1; // Reset decreasing counter
+            } else if (nums[i] < nums[i - 1]) {
+                decLen++;
+                incLen = 1; // Reset increasing counter
+            } else {
+                incLen = 1;
+                decLen = 1;
+            }
+            maxLen = Math.max(maxLen, Math.max(incLen, decLen));
+        }
+
+        return maxLen;
+    }
+}
