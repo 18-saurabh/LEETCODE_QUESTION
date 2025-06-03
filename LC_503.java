47dd41ed5d2a99eb33f98dfd5cf2fd839c2ae794
Saurabh Patel <saurabhpatel4589@gmail.com>
2025-06-03 10:01:21 +0530

    LEETCODE QUESTIONS

---
+++ LC_503.java
@@ -0,0 +1,17 @@
+public class LC_503 {
+     public int[] brute_nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int nge[]=new int [n];
+        for(int i=0;i<n;i++){
+            nge[i]=-1;
+            for(int j=1;j<n;j++){
+                int ind=(i+j)%n;
+                if(nums[ind]>nums[i]){
+                    nge[i]=nums[ind];
+                    break;
+                }
+            }
+        }
+        return nge;
+    }
+}
